f2497c11d80152e2169141bfa409bf5206514955
printyfi <102478248+printyfi@users.noreply.github.com>
2022-06-03 11:52:23 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,38 @@
+// next.config.js
+
+module.exports = {
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  },
+}
+
+
+// next.config.js
+
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+}
+
+
+
+//process.env.NEXT_PUBLIC_API = "https://localhost:3001"
+process.env.NEXT_PUBLIC_API = "https://api.printyfinance.xyz:3005"
+
+//REDIS_HOST=""
+//REDIS_PORT=""
+//REDIS_PASSWORD=""
+
+
+
+//REACT_EDITOR=atom
+
+process.env.NEXT_PUBLIC_CHAINID= "43114"
+
+Authorization = "QTdEQTlBMDUwNzMxMTE3MDBFNDcyMTEwODBCOUE5RkEyMzFFNjMyMDhEMTc0NjQ1MEJGMkZDREVCNTU4OTlFQTowQTZDMkQyMkYxNDcwOTNFQ0NERUFFMzE4MTQ5NUE2RjUyNkUzREI1NzBDMkVFQTkzREI5QzEwOEZBQkNFOTc5"
+
+//process.env.NEXT_PUBLIC_API= "http://api.printy.fi:443"
+process.env.NEXT_PUBLIC_API_TOKEN= "QTdEQTlBMDUwNzMxMTE3MDBFNDcyMTEwODBCOUE5RkEyMzFFNjMyMDhEMTc0NjQ1MEJGMkZDREVCNTU4OTlFQTowQTZDMkQyMkYxNDcwOTNFQ0NERUFFMzE4MTQ5NUE2RjUyNkUzREI1NzBDMkVFQTkzREI5QzEwOEZBQkNFOTc5"
+
