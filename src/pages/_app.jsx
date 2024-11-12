036ba2804d599d02e52219de104313ec817d8fe3
printyfi <102478248+printyfi@users.noreply.github.com>
2024-11-12 18:50:19 +0100

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react' 
 import PropTypes from 'prop-types'
 import Head from 'next/head'
 import Script from 'next/script'
